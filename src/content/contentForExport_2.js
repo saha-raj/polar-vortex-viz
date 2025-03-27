d8242aba0d2b3b939336ccb94c3bf17002a4b989
Raj Saha <99457812+saha-raj@users.noreply.github.com>
2025-03-27 07:51:14 -0400

    Update contentForExport_2.js

--- src/content/contentForExport_2.js
+++ src/content/contentForExport_2.js
@@ -1,9 +1,8 @@
 export const sceneContent = {
     "header-0": "Visualizing the Polar \nJet Stream",
-    "segment-0": `This project, in its current state, shows a visualization of the polar jet stream overlaid on temperature data from the winter of 2010. The motivation of this project is to show how the polar jet stream has been meandering more in recent years due to climate change. 
+    "segment-0": `This project, in its current state, visualizes the polar jet stream and surface temperatures from February 2010. The motivation of this project is to show how the polar jet stream has been meandering more in recent years due to climate change. 
     
-The circumpolar path of the jet stream is computed with numerical simulations (not part of the visualization) using real wind data. 
-    `,
+The circumpolar path of the jet stream is computed with numerical simulations (not part of the visualization) using real wind data`,
 
     "header-1": "The Polar Vortex",
     "segment-1": `The so-called Polar Vortex phenomenon in recent years has come to be associated with the extreme cold temperatures in the the northern hemisphere, during the winter months. This has been attributed
